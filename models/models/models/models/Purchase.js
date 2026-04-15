92458b23deef3515132b65176c8d676ca20d87b8
bindhanibikash71-dot <bindhanibikash71@gmail.com>
2026-04-15 13:12:43 +0530

    Add Purchase model with schema and index

---
+++ models/models/models/models/Purchase.js
@@ -0,0 +1,16 @@
+javascript
+import mongoose from 'mongoose'
+
+const PurchaseSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+  orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
+  purchasedAt: { type: Date, default: Date.now }
+})
+
+PurchaseSchema.index({ userId: 1, courseId: 1 }, { unique: true })
+
+export default mongoose.models.Purchase || mongoose.model('Purchase', PurchaseSchema)
+```
+
+---
