544970e4e8e7a0daae35cd949d0f91ede991dfc2
bindhanibikash71-dot <bindhanibikash71@gmail.com>
2026-04-15 13:07:35 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+javascript
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+---
